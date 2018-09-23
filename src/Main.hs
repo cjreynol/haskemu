57fba4866e5c2fc65ede6e6073ebcc51ad5e9332
Chad Reynolds <cjreynol13@aol.com>
2018-09-22 19:40:45 -0500

    Initial commit

---
+++ src/Main.hs
@@ -0,0 +1,16 @@
+{-|
+Module      : Main
+Description : The main loop
+Copyright   : (c) Chad Reynolds, 2018
+License     : MIT
+-}
+
+module Main (
+    main
+    ) where
+
+
+-- | Main loop
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
+
