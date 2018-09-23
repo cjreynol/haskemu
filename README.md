892b80804593703fe404718057f2f5b02cbcb6c9
Chad Reynolds <cjreynol13@aol.com>
2018-09-23 00:34:27 -0500

    Add some very early work on CHIP-8 emulator
    See the README changes for why I am starting work on this instead of
    the GB emulator.

--- README.md
+++ README.md
@@ -2,7 +2,17 @@
 
 This is a learning project for me, meant to have a larger scope than any of 
 my previous Haskell projects and help me learn how to put them together on a 
-bigger scale.
+bigger scale.  My overall goal is to create a Nintendo Gameboy emulator 
+capable of playing a number of standard ROM files, hopefully expanding this 
+list over time.
+
+Currently, my initial step is to create a 
+[CHIP-8](https://en.wikipedia.org/wiki/CHIP-8) emulator.  After some reading 
+I took some advice from forums and decided to start with a slightly 
+smaller-scoped project and learn some of the basics before taking on the more 
+complicated GB emulator.  My plan is to figure out a good representation for 
+the pieces of the emulator like registers, memory, etc and the best way to 
+fit these together and operate on them.
 
 
 ## Building
--- src/Main.hs
+++ src/Main.hs
@@ -9,8 +9,56 @@ module Main (
     main
     ) where
 
+import Control.Monad.Primitive          (PrimState)
+import Data.Bits
+import Data.Vector              as V
+import Data.Vector.Mutable      as MV
+import Data.Word                        (Word8, Word16)
 
--- | Main loop
+
+-- | Initialize the program state, load the program into memory, then begin
+-- execution loop.
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    regs <- makeRegisters
+    MV.write regs 10 0x00
+    MV.write regs 11 0xAA
+    fRegs <- V.freeze regs
+    putStrLn $ w16ToStr (makeWord16 (fRegs V.! 10) (fRegs V.! 11))
+
+w16ToStr :: Word16 -> String
+w16ToStr (0x00AA) = "matched"
+w16ToStr n      = "unmatched" Prelude.++ show n
+
+makeWord16 :: Word8 -> Word8 -> Word16
+makeWord16 w1 w2 = ((fromIntegral w1) `shiftL` 8) .|. (fromIntegral w2)
+
+
+type Registers = IO (MVector (PrimState IO) Word8)
+makeRegisters :: Registers
+makeRegisters = MV.new 16
+
+type Memory = IO (MVector (PrimState IO) Word8)
+makeMemory :: Memory
+makeMemory = MV.new 4096
+
+data ProgramState = ProgramState {
+      registers         :: Registers
+    , memory            :: Memory
+    , indexReg          :: Word16
+    , programCounter    :: Word16
+    , stack             :: [Word16]
+    , stackPointer      :: Word16
+    , delayTimer        :: Word16
+    , soundTimer        :: Word16
+    }
+
+
+{-
+TODO:
+-implement 35 opcodes (16 bits each)            -Word16 -> actions
+    -pattern match
+    -take actions
+-screen 64x32 bits                              -MVector Word64?
+-}
 
