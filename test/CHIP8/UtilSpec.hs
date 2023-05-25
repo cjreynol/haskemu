c7699ae5bf2f34cfe0ea04753e6ef703021e0160
Chad Reynolds <cjreynol13@aol.com>
2023-05-24 21:12:37 -0700

    Add stub of a test suite
    I want to expand the testing so I can verify behavior as I refactor without
    running the entire program, so this is the first step towards that.

---
+++ test/CHIP8/UtilSpec.hs
@@ -0,0 +1,21 @@
+{-|
+Module      : Util
+Description : The utility function tests
+Copyright   : (c) Chad Reynolds, 2018-2023
+License     : MIT
+-}
+
+module UtilSpec (
+  spec
+) where
+
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+import CHIP8.Util (decrementToZero)
+
+spec :: Spec
+spec = do
+  describe "Util tests" $ do
+    describe "decrementToZero tests" $ do
+      it "placeholder" $ do
+        decrementToZero 0x01 `shouldBe` 0x00
