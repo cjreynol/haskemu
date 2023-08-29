2345c5ff337b1472b65a37b7b31a81742d699402
Chad Reynolds <cjreynol13@aol.com>
2023-08-28 20:47:17 -0700

    WIP disassembler
    Not functioning with a test file I have, but I have no verification around
    that data.

---
+++ haskemu/CHIP8-debugger/Main.hs
@@ -0,0 +1,44 @@
+{-|
+Module      : Main
+Description : The CHIP-8 debugging utility
+Copyright   : (c) Chad Reynolds, 2023
+License     : MIT
+-}
+
+module Main (
+  main
+  ) where
+
+import System.Environment     (getArgs)
+import Data.ByteString as BS  (ByteString, readFile, unpack)
+import Data.Word              (Word8, Word16)
+
+import Opcode                 (Opcode(..), getOpcode)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  (commandStr, filePath) <- verifyArgs args
+  let command = parseCommand commandStr
+  programData <- BS.readFile filePath
+  runCommand command $ BS.unpack programData
+  
+data Command = Disassemble
+  deriving (Show)
+
+verifyArgs :: [String] -> IO (String, String)
+verifyArgs (x:y:_) = pure (x, y)
+verifyArgs _ = error "invalid number of arguments, 2 expected"
+
+parseCommand :: String -> Command
+parseCommand "disassemble" = Disassemble
+parseCommand x = error $ "invalid command " ++ x
+
+runCommand :: Command -> [Word8] -> IO ()
+runCommand Disassemble d = print $ disassemble d
+
+
+disassemble :: [Word8] -> [Opcode]
+disassemble (x:y:xs) = getOpcode x y : disassemble xs
+disassemble [] = []
+disassemble _ = error "Uneven number of bytes in program"
